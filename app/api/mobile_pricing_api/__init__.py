3bb7dfcb78cddd93e6b27aba8f06d2a98d8336d4
JJerome-NM <jjerome010@gmail.com>
2024-01-12 19:40:44 +0200

    [HOTFIX] Fixed small bug😡

--- app/api/mobile_pricing_api/__init__.py
+++ app/api/mobile_pricing_api/__init__.py
@@ -1,9 +1,8 @@
 from flask import Blueprint
-from marshmallow import ValidationError
 import sys
 
 
-sys.path.append("./app/api/mobile_pricing_api/model/Mobile_Pricing")
+sys.path.append("./app/api/mobile_pricing_api/models/Mobile_Pricing")
 
 mobile_pricing_bp = Blueprint('mobile_pricing', __name__)
 
