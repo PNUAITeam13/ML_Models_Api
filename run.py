002578d237e87bd81bfecb7485516d902dc6d01c
JJerome-NM <jjerome010@gmail.com>
2024-01-21 18:56:19 +0200

    [HOTFIX] Small changes for deploy

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
+from os import environ
+
 from app import app
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=environ.get("PORT") or "5000", host=environ.get("HOST") or "localhost")
